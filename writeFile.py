2f977ede294d6c319c04bb7fa54962290a78c933
finfinley <alexfin@outlook.com>
2020-05-28 13:21:36 -0400

    commit on 'master'

---
+++ writeFile.py
@@ -0,0 +1,13 @@
+from lxml import html
+# This file collects the Soup from the page of a tweet and writes
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://twitter.com/realDonaldTrump/status/1265774767493148672/likes'
+html_text = requests.get(url).text
+
+soup = BeautifulSoup(html_text, 'html.parser')
+html = soup.prettify("utf-8")
+with open("soup2.txt", "wb") as file:
+    file.write(html)
+
