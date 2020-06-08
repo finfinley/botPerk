2f1662b716abf6cd03f0777d0ad72c831e89fa34
finfinley <alexfin@outlook.com>
2020-06-08 16:28:57 -0400

    tests to gather more username data

--- botPerkforpi.py
+++ botPerkforpi.py
@@ -1,19 +1,17 @@
-# TODO: Implement headless for selenium
-
-import requests
-import time
-import botometer
+import requests, time, botometer, sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from lxml import html
+from twython import Twython
+
 #!/usr/bin/python
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-url = ("https://twitter.com/realDonaldTrump/status/1265827032707072000")
-driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+url = ("https://twitter.com/realDonaldTrump/status/1268888880264155136")
+driver = webdriver.Chrome(executable_path='C:/Users/alexf/OneDrive/Documents/Python Projects/chromedriver.exe')
 
-# Twitter API / Bot-o-meter Info
+# Bot-o-meter Info
 rapidapi_key = "d8f34cfb8bmshcc45a662c421371p1ab8ddjsn33c9d9b68e66"
 twitter_app_auth = {
     'consumer_key': '3YI6rE8z8naJtUZmFn3Fr1VZF',
@@ -25,8 +23,18 @@ bom = botometer.Botometer(wait_on_ratelimit=True,
                           rapidapi_key=rapidapi_key,
                           **twitter_app_auth)
 
+#Twitter API Info
+apiKey = '3YI6rE8z8naJtUZmFn3Fr1VZF'
+apiSecret = 'kspiJwiRaQm1ou3RnYMbXS3riTCYNm6DCEbrsqdELgCyfvKzry'
 
-def scroll_to_bottom(driver):
+accessToken = '1265347900449730561-BmJNnFLhyLY16su199eiw217LDU35Q'
+accessTokenSecret = 'zavatHUoEbf96OXD8RgZoat416MGSFe0529vTZYgh9NZt'
+
+api = Twython(apiKey, apiSecret, accessToken, accessTokenSecret)
+
+
+
+def scroll_to_bottom(driver,scrolltime):
         print('Beginning hunt...')
         # Empty list for all the usernames scraped
         FULL_LIST = []
@@ -41,11 +49,13 @@ def scroll_to_bottom(driver):
                 ("return (window.pageYOffset !== undefined) ?"
                 " window.pageYOffset : (document.documentElement ||"
                 " document.body.parentNode || document.body);"))
+
                 # Scrapes usernames from current scrolls
                 html = driver.page_source
                 soup = BeautifulSoup(html, 'html.parser')
                 usernameSoup = soup.find_all(
                 "div", class_="css-901oao")
+
                 #css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0
                 list_of_usernames = [x.text for x in usernameSoup]
                 matching = [s for s in list_of_usernames if '@' in s]
@@ -59,7 +69,8 @@ def scroll_to_bottom(driver):
                 FULL_LIST = list(dict.fromkeys(FULL_LIST))
     
                 # Sleep and Scroll
-                time.sleep(20)
+                time.sleep(scrolltime)
+
                 driver.execute_script((
                 "var scrollingElement = (document.scrollingElement ||"
                 " document.body);scrollingElement.scrollTop ="
@@ -74,21 +85,30 @@ def scroll_to_bottom(driver):
         for screen_name, results in bom.check_accounts_in(FULL_LIST):
             print('Busy hunting bots...')
             try:
-                if results['display_scores']['user'] > 3.0:
+                if results['display_scores']['universal'] > 3.0 :
                         bots.append(results['user']['screen_name'])
                         print('Bot found.')
             except KeyError:
+                print('Error digging for account information.')
                 pass
-            
+
         print("Total Bots: ", len(bots))
         print("Bot accounts: ", bots)
-        #print("Results: ", results['display_scores']['user'])
+        print("Out of {}",format(len(FULL_LIST)))
 
+        tweetStr = url + " Total bots found: " + str(len(bots)) + " out of " + str(len(FULL_LIST))
 
+        api.update_status(status = tweetStr)
+        print("Tweet sent.")
+        
 
 driver.get(url)
-scroll_to_bottom(driver)
-#driver.quit()
+scroll_to_bottom(driver, 10)
+driver.get(url)
+scroll_to_bottom(driver, 20)
+driver.get(url)
+scroll_to_bottom(driver, 30)
+driver.quit()
 
 
 
